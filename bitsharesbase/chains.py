fd6cbe1dd95efd3f87a9dc28cc26eb60be00faf3
Graham Rogen <grogen@id-chain.org>
2019-10-06 16:52:16 +0200

    Initial commit

---
+++ bitsharesbase/chains.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+known_chains = {
+    "X4T": {
+        "chain_id": "56f37215a0f68bf8b5ee6e1c5b78b1c6d06bc78e8a69d67a2b1857d1c7a3dde3",
+        "core_symbol": "X4T",
+        "prefix": "X4T",
+    },
+    "TEST": {
+        "chain_id": "39f5e2ede1f8bc1a3a54a7914414e3779e33193f1f5693510e73cb7a87617447",
+        "core_symbol": "TEST",
+        "prefix": "TEST",
+    },
+}
